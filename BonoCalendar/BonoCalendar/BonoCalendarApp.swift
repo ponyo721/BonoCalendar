82ca48c4924c236a72d9b1869bcc13bd9bb2b079
ponyo721 <rhehd721@naver.com>
2024-11-10 17:34:21 +0900

    git init

---
+++ BonoCalendar/BonoCalendar/BonoCalendarApp.swift
@@ -0,0 +1,20 @@
+//
+//  BonoCalendarApp.swift
+//  BonoCalendar
+//
+//  Created by 알파카 on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct BonoCalendarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
